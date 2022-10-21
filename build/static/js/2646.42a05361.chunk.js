5d7c21997389de4e665ffa77d05ac83c18aadfb5
Cong Dang Quoc <cong.dangquoc@asnet.com.vn>
2022-10-22 03:38:09 +0700

    add build

---
+++ build/static/js/2646.42a05361.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktokyo_free_white_react_admin_dashboard=self.webpackChunktokyo_free_white_react_admin_dashboard||[]).push([[2646],{62646:function(e,a,r){r.r(a);r(72791);var t=r(80184);a.default=function(){return(0,t.jsx)("div",{children:"index"})}}}]);
+//# sourceMappingURL=2646.42a05361.chunk.js.map
