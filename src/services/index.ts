1bdc6c689683fcbc8aafb54f8c34f5bd5b0c7129
Cong Dang Quoc <cong.dangquoc@asnet.com.vn>
2022-10-23 21:13:36 +0700

    update default

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,5 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_END_POINT;
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
