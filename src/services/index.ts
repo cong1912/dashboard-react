976089dfabac07243a394cc87fc9cde87a98d01c
Cong Dang Quoc <cong.dangquoc@asnet.com.vn>
2022-10-23 21:25:39 +0700

    fix cors

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,5 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = process.env.REACT_APP_API_END_POINT;
 // axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers['content-type'] = 'application/json;charset=utf-8';
 axios.defaults.headers.post['Content-Type'] =
   'application/x-www-form-urlencoded';
