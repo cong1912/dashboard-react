5279d0cd143de9f1880083318a499b73cb86693b
Cong Dang Quoc <cong.dangquoc@asnet.com.vn>
2022-10-23 21:30:40 +0700

    Update

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -2,5 +2,13 @@ import axios from 'axios';
 import { UserInput } from 'src/interfaces/User';
 
 export const login = (data: UserInput) => {
-  return axios.post('auth/login', data);
+  return axios({
+    method: 'post',
+    url: '/auth/login',
+    data: data,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    }
+  });
 };
